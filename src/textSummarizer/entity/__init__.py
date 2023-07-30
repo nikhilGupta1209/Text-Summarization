161b9ce703231ce35de86b2bbbc4a095bd35cac2
Nikhil Kumar Gupta <nikhilkr496@gmail.com>
2023-07-30 23:59:31 +0530

    added model evaluation, prediction and app.py

--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -41,3 +41,11 @@ class ModelTrainerConfig:
 
 
 
+@dataclass(frozen=True)
+class ModelEvaluationConfig:
+    root_dir: Path
+    data_path: Path
+    model_path: Path
+    tokenizer_path: Path
+    metric_file_name: Path
+
